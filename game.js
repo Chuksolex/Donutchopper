d0b11d45b1f1a0b4c30fb0ff38856862db5ced73
Chukwuma Oleka <75509134+Chuksolex@users.noreply.github.com>
2025-06-23 01:03:25 +0100

    Changed text

--- game.js
+++ game.js
@@ -1,48 +1,3 @@
-// $(document).ready(function () {
-//     let score = 0;
-//     let gameInterval;
-//     let gameDuration = 30000; // 30 seconds
-
-//     $('#start-btn').click(function () {
-//       score = 0;
-//       $('#score').text("Score: 0");
-//       $('#game-area').empty();
-//       clearInterval(gameInterval);
-
-//       const startTime = Date.now();
-//       gameInterval = setInterval(() => {
-//         const now = Date.now();
-//         if (now - startTime >= gameDuration) {
-//           clearInterval(gameInterval);
-//           alert("Game Over! Your score is: " + score);
-//           return;
-//         }
-//         spawnDonut();
-//       }, 800);
-//     });
-
-//     function spawnDonut() {
-//       const donut = $('<div class="donut"></div>');
-//       const posX = Math.random() * 250; // 300 - 50
-//       donut.css({ left: posX + 'px', top: '0px' });
-//       $('#game-area').append(donut);
-
-//       const fallSpeed = Math.random() * 3000 + 2000;
-
-//       donut.animate({ top: '500px' }, fallSpeed, function () {
-//         $(this).remove();
-//       });
-
-//       donut.click(function () {
-//         score++;
-//         $('#score').text("Score: " + score);
-//         $(this).stop().fadeOut(200, function () {
-//           $(this).remove();
-//         });
-//       });
-//     }
-//   });
-//1. Get click to start game
 
 
 let score = 0;
@@ -50,7 +5,7 @@ let gameInterval;
 let gameDuration = 30000;
 //declaration of variables
 
-$("#start-btn").click(function () {
+$("#start-btn").click(function () { // Clicking of Button To Start the game
   const startTime = Date.now();
 
   gameInterval = setInterval(function () {
@@ -64,14 +19,14 @@ $("#start-btn").click(function () {
 
                  createDonut();
                  
-               }, 800);
+               }, 800);// Create A new donout after every 0.8secs
 });
 
 ///start button ended here
 
 function createDonut() {
-  const donut = $("<img class='donut' src='donut.png'>");
-  const positionX = Math.random() * 250;
+  const donut = $("<img class='donut' src='donut.png'>"); //bring the donut into the dom
+  const positionX = Math.random() * 250;  // Put the donut in a random position on x-axis inside the 300px with
   donut.css({ left: positionX + "px", top: "0px" });
   $("#game-area").append(donut);
 
